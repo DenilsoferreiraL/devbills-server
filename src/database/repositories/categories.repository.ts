4f0f538db013e1209dbcb5e72f7bb9d874e7456d
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2024-10-02 12:35:52 -0300

    feat: create CategoriesRepository and Category schema, change id to _id.

---
+++ src/database/repositories/categories.repository.ts
@@ -0,0 +1,14 @@
+import { Category } from "../../entities/category.entity";
+import { CategoryModel } from "../schemas/category.schema";
+
+export class CategoriesRepository {
+    constructor(private model: typeof CategoryModel) {
+
+    }
+
+    async create({ title, color }: Category): Promise<Category> {
+        const createdCategory = await this.model.create({ color, title })
+
+        return createdCategory.toObject<Category>()
+    }
+}
