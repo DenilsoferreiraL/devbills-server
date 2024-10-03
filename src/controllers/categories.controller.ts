b82e5d8c7b82d99daa16fb65fe4171c4a93999ef
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2024-10-03 08:58:23 -0300

    feat: add error handling with try-catch in category creation and apply global errorHandler middleware.

--- src/controllers/categories.controller.ts
+++ src/controllers/categories.controller.ts
@@ -1,18 +1,22 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { CategoriesService } from "../services/categories.service";
 import { CategoriesRepository } from "../database/repositories/categories.repository";
 import { CategoryModel } from "../database/schemas/category.schema";
 import { CreateCategoryDTO } from "../dtos/categories.dto";
 export class CategoriesController {
-    async create(req: Request<unknown, unknown, CreateCategoryDTO>, res: Response) {
+    async create(req: Request<unknown, unknown, CreateCategoryDTO>, res: Response, next: NextFunction) {
 
-        const { title, color } = req.body
+        try {
+            const { title, color } = req.body
 
-        const repository = new CategoriesRepository(CategoryModel)
-        const service = new CategoriesService(repository)
+            const repository = new CategoriesRepository(CategoryModel)
+            const service = new CategoriesService(repository)
 
-        const result = await service.create({ title, color })
+            const result = await service.create({ title, color })
 
-        return res.status(201).json(result)
+            return res.status(201).json(result)
+        } catch (err) {
+            next(err)
+        }
     }
 }
