ab1c722e8b8cc3bf8ac6a2eb6ef57450f1d2325b
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2024-10-03 09:23:00 -0300

    feat: add zod schema for validating category creation input.

--- src/controllers/categories.controller.ts
+++ src/controllers/categories.controller.ts
@@ -3,10 +3,19 @@ import { CategoriesService } from "../services/categories.service";
 import { CategoriesRepository } from "../database/repositories/categories.repository";
 import { CategoryModel } from "../database/schemas/category.schema";
 import { CreateCategoryDTO } from "../dtos/categories.dto";
+import { z } from "zod";
 export class CategoriesController {
     async create(req: Request<unknown, unknown, CreateCategoryDTO>, res: Response, next: NextFunction) {
 
         try {
+
+            const validadeSchema = z.object({
+                title: z.string(),
+                color: z.string().regex(/^#[A-Fa-f0-9]{6}$/)
+            })
+
+            validadeSchema.parse(req.body)
+
             const { title, color } = req.body
 
             const repository = new CategoriesRepository(CategoryModel)
