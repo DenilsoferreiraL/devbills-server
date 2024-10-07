0147c61e4fb8870f8edd5be7b95907c56635bcb7
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2024-10-07 15:27:49 -0300

    feat: add schema validation for transaction creation using Zod.

---
+++ src/dtos/transactions.dto.ts
@@ -0,0 +1,10 @@
+import { z } from "zod"
+
+export const createTransactionsSchema = {
+    title: z.string(),
+    amount: z.number().int().positive(),
+    type: z.string(),
+    data: z.coerce.date(),
+    categoryId: z.string()
+}
+
