a4449ff5897f1c36a506a14a9830e5edf327bd2b
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2024-10-08 11:53:55 -0300

    feat: add TransactionsService with create method.

---
+++ src/services/transactions.service.ts
@@ -0,0 +1,9 @@
+import { TransactionsRepository } from "../database/repositories/transactions.repository";
+
+export class TransactionsService {
+    constructor(private TransactionsRepository: TransactionsRepository) { }
+
+    async create(): Promise<Transaction> {
+
+    }
+}
