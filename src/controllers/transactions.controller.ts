9fa1d63e2fa03808890727199e124fd7f2c5a27b
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2024-10-09 12:53:58 -0300

    feat: create TransactionController with create method.

---
+++ src/controllers/transactions.controller.ts
@@ -0,0 +1,33 @@
+import { NextFunction, Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import { TransactionsService } from "../services/transactions.service";
+import { CreateTransactionDTO } from "../dtos/transactions.dto";
+
+export class TransactionController {
+
+    constructor(private transactionsService: TransactionsService) { }
+
+    create = async (req: Request<unknown, unknown, CreateTransactionDTO>, res: Response, next: NextFunction) => {
+
+        try {
+            const { title, amount, categoryId, date, type } = req.body
+
+            const result = await this.transactionsService.create({ title, amount, categoryId, date, type })
+
+            return res.status(StatusCodes.CREATED).json(result)
+        } catch (err) {
+            next(err)
+        }
+    }
+
+    // index = async (_: Request, res: Response, next: NextFunction) => {
+
+    //     try {
+    //         const result = await this.transactionsService.index()
+
+    //         return res.status(StatusCodes.OK).json(result)
+    //     } catch (err) {
+    //         next(err)
+    //     }
+    // }
+}
