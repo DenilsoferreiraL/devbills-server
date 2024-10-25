ffedbdb1dbbe47c93270a915ec44607eb5884d31
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2024-10-25 16:29:24 -0300

    feat: implement getDashboard endpoint to retrieve balance summary.

--- src/controllers/transactions.controller.ts
+++ src/controllers/transactions.controller.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import { TransactionsService } from "../services/transactions.service";
-import { CreateTransactionDTO, IndexTransactionsDTO } from "../dtos/transactions.dto";
+import { CreateTransactionDTO, GetDashboardDTO, IndexTransactionsDTO } from "../dtos/transactions.dto";
 
 export class TransactionController {
 
@@ -26,6 +26,18 @@ export class TransactionController {
         try {
             const { title, categoryId, beginDate, endDate } = req.query
             const result = await this.transactionsService.index({ title, categoryId, beginDate, endDate })
+
+            return res.status(StatusCodes.OK).json(result)
+        } catch (err) {
+            next(err)
+        }
+    }
+
+    getDashboard = async (req: Request<unknown, unknown, unknown, GetDashboardDTO>, res: Response, next: NextFunction) => {
+
+        try {
+            const { beginDate, endDate } = req.query
+            const result = await this.transactionsService.getDashboard({ beginDate, endDate })
 
             return res.status(StatusCodes.OK).json(result)
         } catch (err) {
